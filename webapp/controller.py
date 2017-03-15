28b8c7fa7b0a7c28565f8a436278316e51427a78
tweddielin <power6188@gmail.com>
2017-03-15 01:46:03 -0500

    seperate plot from controller

--- webapp/controller.py
+++ webapp/controller.py
@@ -1,10 +1,5 @@
 from flask import render_template, request, url_for, jsonify
-from webapp import app, model
-from bokeh.charts import Bar
-from bokeh.embed import components
-from bokeh.models import Range1d
-from bokeh.charts.attributes import CatAttr
-import bokeh.plotting as plt
+from webapp import app, model, draw
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -14,7 +9,7 @@ def index():
             result = model.find_most_similar_topic(posttext, 10)
             labels = list(map(lambda i: i[0], result))
             values = list(map(lambda i: i[1], result))
-            script, div = make_bar_plot(labels, values)
+            script, div = draw.make_bar_plot(labels, values)
             print(result)
             return render_template("index.html", results=result, script=script, div=div)
     return render_template("index.html")
@@ -29,9 +24,3 @@ def get_similar(word, topn):
         print('there are some problems')
         return jsonify({"sorry": "Sorry, no results! Please try again."}), 500
 
-def make_bar_plot(labels, values):
-    data = {'labels': labels, 'values': values}
-    bar = Bar(data, values='values', label=CatAttr(columns=['labels'], sort=False), legend=False, agg='mean', bar_width=0.6, plot_width=500, plot_height=350)
-    bar.y_range = Range1d(0.0, 1.0)
-    script, div = components(bar)
-    return script, div
